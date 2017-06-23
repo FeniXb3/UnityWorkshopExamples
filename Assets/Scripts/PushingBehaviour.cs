68bfb9379154bf82811cf6acad31809c3b063cff
FeniXb3 <fenix.b3@gmail.com>
2017-06-23 08:10:03 +0200

    Show pushing force vector in editor

--- Assets/Scripts/PushingBehaviour.cs
+++ Assets/Scripts/PushingBehaviour.cs
@@ -2,10 +2,11 @@
 
 public class PushingBehaviour : MonoBehaviour
 {
+    public Vector3 forceVector = new Vector3(0f, 0f, 10f);
+
 	void Start()
     {
 		var rigidBody = GetComponent<Rigidbody>();
-		var forceVector = new Vector3(0f, 0f, 10f);
 
 		rigidBody.AddForce(forceVector, ForceMode.Impulse);		
 	}
