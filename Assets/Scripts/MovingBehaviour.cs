da916fcd6fb398a683819ffc82abfa6568cb2803
FeniXb3 <fenix.b3@gmail.com>
2017-06-23 17:10:13 +0200

    Add moving Player capsule

---
+++ Assets/Scripts/MovingBehaviour.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class MovingBehaviour : MonoBehaviour
+{
+    CharacterController controller;
+
+	void Start ()
+    {
+        controller = GetComponent<CharacterController>();	
+	}
+	
+	void Update ()
+    {
+        var verticalInput = Input.GetAxis("Vertical");
+        var horizontalInput = Input.GetAxis("Horizontal");
+
+        var velocity = transform.forward * verticalInput;
+
+        transform.Rotate(transform.up, horizontalInput);
+        controller.SimpleMove(velocity);
+	}
+}
