43589402d7790ba9de5307cbc60f96a3b6730c6f
FeniXb3 <fenix.b3@gmail.com>
2017-06-23 08:07:16 +0200

    Clean pushing code

--- Assets/Scripts/PushingBehaviour.cs
+++ Assets/Scripts/PushingBehaviour.cs
@@ -1,19 +1,12 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
-public class PushingBehaviour : MonoBehaviour {
-
-	// Use this for initialization
-	void Start() {
+public class PushingBehaviour : MonoBehaviour
+{
+	void Start()
+    {
 		var rigidBody = GetComponent<Rigidbody>();
 		var forceVector = new Vector3(0f, 0f, 10f);
 
 		rigidBody.AddForce(forceVector, ForceMode.Impulse);		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
 	}
 }
