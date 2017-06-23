524752cc1983c66982124b1e84c4082d034ddd8c
FeniXb3 <fenix.b3@gmail.com>
2017-06-23 08:03:55 +0200

    Add cube pushed with force impulse

---
+++ Assets/Scripts/PushingBehaviour.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PushingBehaviour : MonoBehaviour {
+
+	// Use this for initialization
+	void Start() {
+		var rigidBody = GetComponent<Rigidbody>();
+		var forceVector = new Vector3(0f, 0f, 10f);
+
+		rigidBody.AddForce(forceVector, ForceMode.Impulse);		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
