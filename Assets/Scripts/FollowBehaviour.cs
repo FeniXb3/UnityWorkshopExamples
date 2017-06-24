67be46cd92e067850b45549c2f5867e10bff984f
FeniXb3 <fenix.b3@gmail.com>
2017-06-24 15:15:15 +0200

    Display dots and terrain on minimap

---
+++ Assets/Scripts/FollowBehaviour.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowBehaviour : MonoBehaviour 
+{
+    public GameObject target;
+
+	private void LateUpdate()
+	{
+        if (target != null)
+        {
+            transform.position = target.transform.position;
+        }
+	}
+}
---
+++ Assets/Scripts/MinimapDotCreatingtBehaviour.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class MinimapDotCreatingtBehaviour : MonoBehaviour 
+{
+    public Color dotColor = Color.white;
+    public GameObject target;
+    public GameObject dotGO;
+
+	void Start () 
+    {
+        if(target == null)
+        {
+            target = gameObject;
+        }
+
+        var dotPrefab = Resources.Load("Prefabs/Minimap Dot", typeof(GameObject)) as GameObject;
+        dotGO = Instantiate(dotPrefab);
+        dotGO.name = string.Format("{0} - {1}", dotPrefab.name, target.name);
+
+        dotGO.GetComponent<SpriteRenderer>().color = dotColor;
+        dotGO.GetComponent<FollowBehaviour>().target = gameObject;
+	}
+}
